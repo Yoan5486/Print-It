ed3d39a058a27a63088010d6ef5df2191ea661f7
Yoan Cocquebert <yoan.principal@gmail.com>
2024-02-19 17:44:57 +0100

    Défilement des images et textes fonctionnels

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,72 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-let balisearrow_left = document.querySelector(".arrow_left");
-console.log(balisearrow_left)
+let baliseArrowLeft = document.querySelector(".arrow_left");
+console.log(baliseArrowLeft)
 
-balisearrow_left.addEventListener("click", () => {
+let baliseArrowRight = document.querySelector(".arrow_right");
+console.log(baliseArrowRight)
+
+
+let baliseImage = document.querySelector(".banner-img");
+
+let bannerTagLine = document.querySelector("#banner p");
+
+let dotSelected = document.querySelector(".dot_selected");
+
+let currentSlide = 0;
+let 
+
+
+function updateSlides() {
+	baliseImage.src = slides[currentSlide].image;
+	bannerTagLine.innerHTML = slides[currentSlide].tagLine
 	
-	console.log(balisearrow_left)
+}
+
+baliseArrowLeft.addEventListener("click", () => {
+	currentSlide--
+	console.log(baliseArrowLeft)
+	updateSlides ();
+	
+		
 });
 
-let balisearrow_right = document.querySelector(".arrow_right");
-console.log(balisearrow_right)
+baliseArrowRight.addEventListener ("click", () => {
+	currentSlide++
+	updateSlides ();
+		
+	});
+
+
+console.log (baliseArrowLeft)
 
 
 
-
-console.log(balisearrow_right)
-
-console.log (balisearrow_left)
+let dotsContainer = document.querySelector(".dots");
 
 
-let banner_tagline = document.querySelector("#banner p");
 
-let baliseimage = document.querySelector(".banner-img");
-
-let dot_selected = document.querySelector(".dot_selected");
-
-let dots_container = document.querySelector(".dots");
-
-console.log(baliseimage)
 
 for (let i = 0; i < slides.length; i++) {
 	 
-	dots_container.innerHTML += '<div class="dot"></div>'
+	dotsContainer.innerHTML += '<div class="dot"></div>';
 
-	balisearrow_right.addEventListener ("click", () => {
-		baliseimage.forEach((img, i) => {
-			img.src = "./assets/images/slideshow" + slides[index].image;
-			console.log(img.src)
-		});
-
-		banner_tagline.innerHTML = slides[index].image = tagLine;
 	
-})
-};
+}
