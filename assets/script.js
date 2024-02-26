400b19dee632632a3ba4cac5a153d6ad4552636e
Yoan Cocquebert <yoan.principal@gmail.com>
2024-02-26 17:43:57 +0100

    Finalisation du Projet

--- assets/script.js
+++ assets/script.js
@@ -17,23 +17,51 @@ const slides = [
 	}
 ]
 
-let baliseArrowLeft = document.querySelector(".arrow_left");
+let baliseArrowLeft = document.querySelector(".arrow_left")
 console.log(baliseArrowLeft)
 
-let baliseArrowRight = document.querySelector(".arrow_right");
+let baliseArrowRight = document.querySelector(".arrow_right")
 console.log(baliseArrowRight)
 
-let baliseImage = document.querySelector(".banner-img");
+let baliseImage = document.querySelector(".banner-img")
 
-let bannerTagLine = document.querySelector("#banner p");
+let bannerTagLine = document.querySelector("#banner p")
 
-let currentSlide = 0;
+let currentSlide = 0
+
+let dotsContainer = document.querySelector(".dots");
 
 function updateSlides() {
 	baliseImage.src = slides[currentSlide].image;
 	bannerTagLine.innerHTML = slides[currentSlide].tagLine
 }
 
+let dotSelected = document.querySelector(".dots")
+
+function createDots() {
+	for (let i = 0; i < slides.length; i++) {
+		dotsContainer.innerHTML += '<div class="dot"></div>'
+}
+let allDots = document.querySelectorAll(".dot")
+allDots[0].classList.add("dot_selected")
+}
+
+createDots ();
+
+function dotSelectedSlide() {
+	let allDots = document.querySelectorAll(".dot")
+	allDots.forEach ((dot, index) => {
+		if (index===currentSlide) {
+			dot.classList.add("dot_selected")
+		}
+		
+		else {
+			dot.classList.remove("dot_selected")
+		}
+	}) 
+}
+
+dotsContainer.innerHTML += '<div class="dot dot_selected></div>'
 
 baliseArrowLeft.addEventListener("click", () => {
 	currentSlide --
@@ -44,7 +72,8 @@ baliseArrowLeft.addEventListener("click", () => {
 	else {
 		
 	};	
-	updateSlides ();
+	updateSlides ()
+	dotSelectedSlide ()
 	console.log(currentSlide)
 });
 
@@ -56,37 +85,9 @@ baliseArrowRight.addEventListener ("click", () => {
 	else {
 	
 	};
-	updateSlides ();
+	updateSlides ()
+	dotSelectedSlide ()
 	console.log(currentSlide)
 });
 
 console.log (baliseArrowLeft)
-
-let dotSelected = document.querySelector(".dots")
-
-function dotSelectedSlide() {
-	dotSelected.classList.add("dot_selected")
-}
-
-
-
-
-
-let dotsContainer = document.querySelector(".dots");
-
-for (let i = 0; i < slides.length; i++) {
-		dotsContainer.innerHTML += '<div class="dot"></div>';
-}
-if (i = currentSlide) {
-	dotsContainer.innerHTML += '<div class="dot dot_selected></div>';
-}
-
-else {
-	
-}
-
-/*for (let j = 0; currentSlide >= 0; j=currentSlide) {
-		dotsContainer.innerHTML += '<div class="dot dot_selected"></div>';
-}*/
-
-console.log(dotSelected); 
